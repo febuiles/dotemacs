1db8113b54de3c1277f3c921f9a16423aac16f9e
Federico Builes <federico.builes@gmail.com>
2008-07-22 09:53:46 -0500

    Key bindings

--- hooks.el
+++ hooks.el
@@ -1,19 +1,17 @@
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 (add-hook 'ruby-mode-hook '(lambda () (ruby-electric-mode)))
-;; (add-hook 'c-mode-common-hook
-;;           (lambda ()
-;;             (c-set-style "k&r")
-;;             (setq c-basic-offset 4)))
 (add-hook 'text-mode-hook 'turn-on-auto-fill)
 (add-hook 'c++-mode-hook 'turn-on-auto-fill)
 (add-hook 'c-mode-hook 'turn-on-auto-fill)
 (add-hook 'ruby-mode-hook 'turn-on-auto-fill)
 
-;; Use C-x k to kill emacs client sessions.
+;; Use C-c k to kill emacs client sessions.
 (add-hook 'server-switch-hook 
 	  (lambda ()
-	    (local-set-key (kbd "C-x C-k") 'server-edit)))
+	    (local-set-key (kbd "C-c k") 'server-edit)))
+
+
 
 
 
