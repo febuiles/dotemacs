2280ef5e2f66305afa62399fc7634994f43aaddc
Federico Builes <federico.builes@gmail.com>
2008-07-28 09:52:44 -0500

    New files

---
+++ mail.el
@@ -0,0 +1,12 @@
+(load "starttls")
+(setq send-mail-function 'smtpmail-send-it
+      message-send-mail-function 'smtpmail-send-it
+      smtpmail-starttls-credentials
+      '(("smtp.gmail.com" 587 nil nil))
+      smtpmail-auth-credentials
+      '(("smtp.gmail.com" 587 "federico.builes@gmail.com" nil))
+      smtpmail-default-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-service 587
+      smtpmail-debug-info t)
+(require 'smtpmail)
