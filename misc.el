cb3b37f27169894150d1d4a563a88cb52f7a59e3
Federico Builes <federico.builes@gmail.com>
2008-07-30 21:14:04 -0500

    Adding some C stuff.

--- misc.el
+++ misc.el
@@ -14,6 +14,19 @@
 (set-frame-position (selected-frame) 50 40)
 (setq rinari-tags-file-name "TAGS")
 
+(defun linux-c-mode ()
+  "C mode with adjusted defaults for use with the Linux kernel."
+  (interactive)
+  (c-mode)
+  (setq c-indent-level 4)
+  (setq c-brace-imaginary-offset 0)
+  (setq c-brace-offset -4)
+  (setq c-argdecl-indent 4)
+  (setq c-label-offset -4)
+  (setq c-continued-statement-offset 4)
+  (setq indent-tabs-mode nil)
+  (setq tab-width 8))
+
 
 
 
