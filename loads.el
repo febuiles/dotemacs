b763a1a1470561d3cbe453c371379817c624d8a1
Federico Builes <federico.builes@gmail.com>
2008-11-24 11:12:11 -0500

    Updating textmate.el

--- loads.el
+++ loads.el
@@ -20,7 +20,7 @@
 (load "gist")
 (load "haskell-site-file")
 (load "textmate")
-
+(textmate-mode)
 (require 'quack)
 (require 'vc-git)
  (when (featurep 'vc-git) (add-to-list 'vc-handled-backends 'git))
