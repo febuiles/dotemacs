1f0b15b33fbae1599d37b9ea0e2b1bebc7ef9c76
Federico Builes <federico.builes@gmail.com>
2008-12-01 01:38:16 -0500

    Don't set the markdown for *.*.

--- modes.el
+++ modes.el
@@ -1,2 +1,2 @@
 (setq auto-mode-alist (cons '("\\.el\\'" . lisp-interaction-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '("\\'" . markdown-mode) auto-mode-alist))
+;;(setq auto-mode-alist (cons '("\\'" . markdown-mode) auto-mode-alist))
