ff4f94925b117dbe4aaab82bad472efb7add0c22
Federico Builes <federico.builes@gmail.com>
2008-12-24 01:17:40 -0500

    Remove rubydb and rspec abbrevs.

--- vendor/ruby/abbrev_defs.el
+++
@@ -1,6 +0,0 @@
-(define-abbrev-table 'ruby-mode-abbrev-table '(
-    ("itdo" "" rspec-test-skeleton 0)
-    ("describe" "" rspec-describe-skeleton 0)
-    ("beach" "" rspec-before-each-skeleton 0)
-    ))
-
