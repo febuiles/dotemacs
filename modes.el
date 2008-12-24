b06549e11adb8ca392e001aeccaca37a24886489
Federico Builes <federico.builes@gmail.com>
2008-12-23 22:53:19 -0500

    Use HTML mode for normal HTML files.

--- modes.el
+++ modes.el
@@ -6,7 +6,7 @@
 (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.xml$" . nxml-mode))
-(add-to-list 'auto-mode-alist '("\\.html$" . nxhtml-mode))
+(add-to-list 'auto-mode-alist '("\\.html$" . html-mode))
 (add-to-list 'auto-mode-alist '("\\.rhtml$" . nxhtml-mode))
 (add-to-list 'auto-mode-alist '("\\.haml$" . haml-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
