d6ffd45fc017b7773e3815d5f85be260d9ee052f
Federico Builes <federico.builes@gmail.com>
2008-12-24 12:23:27 -0500

    init.el replaces .emacs file.

---
+++ init.el
@@ -0,0 +1,25 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(case-fold-search t)
+ '(markdown-command "Markdown.pl | SmartyPants.pl")
+ '(mouse-wheel-mode t nil (mwheel))
+ '(safe-local-variable-values (quote ((tcl-indent-level . 8))))
+ '(show-paren-mode t nil (paren))
+ '(standard-indent 4)
+ '(transient-mark-mode t)
+ '(visible-bell t))
+
+(setq c-basic-offset 4)
+(setq-default indent-tabs-mode nil)
+(setq mac-allow-anti-aliasing t)
+(load "~/.emacs.d/loads.el")
+
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
