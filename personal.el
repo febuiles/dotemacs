fce5a87ad8a66dceace84cf139b80910143d3b5d
Federico Builes <federico.builes@gmail.com>
2008-12-25 20:16:34 -0500

    Enable the custom directory for modes that do use autosaves.

--- personal.el
+++ personal.el
@@ -17,16 +17,14 @@
 ;; don't clutter the workspace with a bunch of backups
 (defvar user-temporary-file-directory
   (concat temporary-file-directory user-login-name "/"))
-
 (make-directory user-temporary-file-directory t)
 (setq backup-by-copying t)
 (setq backup-directory-alist
-      `(("." . ,user-temporary-file-directory)
-        (,tramp-file-name-regexp nil)))
+      `(("." . ,user-temporary-file-directory)))
+(setq auto-save-list-file-prefix
+      (concat user-temporary-file-directory ".auto-saves-"))
+(setq auto-save-file-name-transforms
+      `((".*" ,user-temporary-file-directory t)))
 (setq auto-save-default nil)
-;; (setq auto-save-list-file-prefix
-;;       (concat user-temporary-file-directory ".auto-saves-"))
-;; (setq auto-save-file-name-transforms
-;;       `((".*" ,user-temporary-file-directory t)))
 
 
