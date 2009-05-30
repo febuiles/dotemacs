407cfb76bf617602439b7fc775dd74fb279dff20
Federico Builes <federico.builes@gmail.com>
2009-05-29 23:02:28 -0500

    Adding JS2-mode.

--- modes.el
+++ modes.el
@@ -16,4 +16,6 @@
 (add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (add-to-list 'auto-mode-alist '("\\.treetop$" . treetop-mode))
- 
+
+(autoload 'js2-mode "js2" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
