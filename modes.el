6c337b71f2dbe0d0f0e94fba2f6dfbd5df2d1fe2
Federico Builes <federico.builes@gmail.com>
2011-02-01 23:25:18 -0500

    Treat Gemfile files as Ruby files.

--- modes.el
+++ modes.el
@@ -11,6 +11,7 @@
 (add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.xml$" . nxml-mode))
 (add-to-list 'auto-mode-alist '("\\.html$" . html-mode))
 (add-to-list 'auto-mode-alist '("\\.rhtml$" . rhtml-mode))
