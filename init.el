a9d1681f34e7e82dfe97933d9fe4ad990c812663
Federico Builes <federico.builes@gmail.com>
2011-02-01 23:25:05 -0500

    Adding some custom variables.

--- init.el
+++ init.el
@@ -86,12 +86,9 @@
 (color-theme-twilight)
 
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
  '(haskell-font-lock-symbols t)
  '(js2-basic-offset 4))
+
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
