8da497c4b8f6e2696d09333ab1a2dc600456ad88
Federico Builes <febuiles@github.com>
2020-06-23 07:40:31 +0200

    Don't use this anymore :)

--- lisp/hooks.el
+++ lisp/hooks.el
@@ -52,10 +52,6 @@
 (add-hook 'coffee-mode-hook
   '(lambda() (set (make-local-variable 'tab-width) 2)))
 
-
-(add-hook 'csharp-mode-hook
-  '(lambda() (setq c-basic-offset 4)))
-
 ;; colors in shells
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
 
