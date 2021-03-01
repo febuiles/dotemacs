bd98ab1ca8692a3d49aff191ad651deff4f6e3d6
Federico Builes <febuiles@github.com>
2021-03-01 20:04:50 +0100

    Update init.el

--- init.el
+++ init.el
@@ -130,5 +130,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (ivy tern swift-mode projectile neotree minitest magit lsp-mode json-mode go-mode gist fzf dumb-jump auto-complete ag))))
+   '(rhtml-mode fzf ivy tern swift-mode projectile neotree minitest magit lsp-mode json-mode go-mode gist dumb-jump auto-complete ag)))
