d9103df510aef851fddbaabbb12a63b763a91d5a
Federico Builes <febuiles@github.com>
2022-01-27 15:11:53 +0100

    Removing out of date Ruby packages.

--- lisp/hooks.el
+++ lisp/hooks.el
@@ -23,12 +23,6 @@
 	  (lambda ()
 	    (local-set-key (kbd "C-c k") 'server-edit)))
 
-;; Load ruby-inf
-(autoload 'inf-ruby "vendor/ruby/inf-ruby" "Run an inferior Ruby process" t)
-(autoload 'inf-ruby-keys "vendor/ruby/inf-ruby" "" t)
-(eval-after-load 'ruby-mode
-  '(add-hook 'ruby-mode-hook 'inf-ruby-keys))
-
 ;; Pre-save hooks (Stack Overflow ftw! http://stackoverflow.com/questions/1214407/how-to-write-a-global-save-hook-for-emacs)
 (defun unix-newline ()
   (set-buffer-file-coding-system 'undecided-unix))
