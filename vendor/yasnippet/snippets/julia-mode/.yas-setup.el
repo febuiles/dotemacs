da21a068bb8754aae068be9bb03a0aaac3ae4593
Federico Builes <febuiles@github.com>
2024-03-21 14:05:33 +0100

    remove a ton of shit I added by mistake

--- vendor/yasnippet/snippets/julia-mode/.yas-setup.el
+++
@@ -1,6 +0,0 @@
-;;; -*- lexical-binding: t -*-
-(require 'yasnippet)
-
-(defun yas-julia-iteration-keyword-choice ()
-  "Choose the iteration keyword for for-loop"
-  (yas-choose-value '("=" "in" "∈")))
